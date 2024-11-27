9d9c93a36c86311a1548917d7c21f9c72b5b8a42
Vasavi Boda <142920395+Vasaviboda@users.noreply.github.com>
2024-11-27 13:07:44 +0530

    Add files via upload

---
+++ front.js
@@ -0,0 +1,117 @@
+function textColorRed(){
+    let t=document.getElementById('text')
+    t.style.color="red"
+}
+function textColorGreen(){
+    let t=document.getElementById('text')
+    t.style.color="green"
+}
+function textColorBlue(){
+    let t=document.getElementById('text')
+    t.style.color="blue"
+}
+function textColorPink(){
+    let t=document.getElementById('text')
+    t.style.color="pink"
+}
+function textColorYellow(){
+    let t=document.getElementById('text')
+    t.style.color="yellow"
+}function textColorPurple(){
+    let t=document.getElementById('text')
+    t.style.color="purple"
+}function textColorMagenta(){
+    let t=document.getElementById('text')
+    t.style.color="magenta"
+}function textColorOrange(){
+    let t=document.getElementById('text')
+    t.style.color="orange"
+}function textColorMaroon(){
+    let t=document.getElementById('text')
+    t.style.color="maroon"
+}function textColorBlack(){
+    let t=document.getElementById('text')
+    t.style.color="black"   
+}
+
+
+
+function backColorRed(){
+    let t=document.getElementById('text')
+    t.style.background="red"
+}
+function backColorGreen(){
+    let t=document.getElementById('text')
+    t.style.background="green"
+}
+function backColorBlue(){
+    let t=document.getElementById('text')
+    t.style.background="blue"
+}
+function backColorPink(){
+    let t=document.getElementById('text')
+    t.style.background="pink"
+}
+function backColorYellow(){
+    let t=document.getElementById('text')
+    t.style.background="yellow"
+}function backColorPurple(){
+    let t=document.getElementById('text')
+    t.style.background="purple"
+}function backColorMagenta(){
+    let t=document.getElementById('text')
+    t.style.background="magenta"
+}function backColorOrange(){
+    let t=document.getElementById('text')
+    t.style.background="orange"
+}function backColorMaroon(){
+    let t=document.getElementById('text')
+    t.style.background="maroon"
+}function backColorBlack(){
+    let t=document.getElementById('text')
+    t.style.background="black"   
+}
+
+
+
+function setFontFamilyGeorgia(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="georgia";   
+
+}
+function setFontFamilyArial(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="arial,sans-serif";   
+
+}
+function setFontFamilyGaramond(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="garamond";   
+
+}
+
+function setFontFamilyVerdana(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="verdana";   
+
+}
+function setFontFamilyCourier(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="courier";   
+
+}
+function setFontFamilyFutura(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="futura";   
+
+}
+function setFontFamilyRoboto(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="roboto";   
+
+}
+function setFontFamilyHelvetica(){
+    let t=document.getElementById('text')
+    t.style.fontFamily="helvetica";   
+
+}
